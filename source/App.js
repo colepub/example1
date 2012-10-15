46f288fb65f88855a0197561d5ee67fcdfb74a01
Cole Davis <cole@coled.net>
2012-10-15 16:00:37 -0400

    add usage of submodule

--- source/App.js
+++ source/App.js
@@ -5,6 +5,7 @@ enyo.kind({
 		{name: "hello", content: "Hello World", allowHtml: true, ontap: "helloWorldTap"}
 	],
 	helloWorldTap: function(inSender, inEvent) {
-		this.$.hello.setContent(new Date().toLocaleTimeString());
+		//this.$.hello.setContent(new Date().toLocaleTimeString());
+        this.$.hello.setContent(GenerateColorTag("hello was tapped"));
 	}
 });
