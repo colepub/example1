61458fe17900f4c30d4346def2aca2566ec1e4a2
Cole Davis <cole@coled.net>
2012-10-15 15:33:33 -0400

    fix spelling

--- source/App.js
+++ source/App.js
@@ -2,7 +2,7 @@ enyo.kind({
 	name: "App",
 	fit: true,
 	components:[
-		{name: "hello", content: "Hello Werld", allowHtml: true, ontap: "helloWorldTap"}
+		{name: "hello", content: "Hello World", allowHtml: true, ontap: "helloWorldTap"}
 	],
 	helloWorldTap: function(inSender, inEvent) {
 		this.$.hello.addContent("<br/><b>hello</b> control was tapped");
